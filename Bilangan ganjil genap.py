8ab9292f7c635e69ee8343da84862dcf11a7c654
Afifaftawi <afifaftawi07@gmail.com>
2024-09-13 09:51:54 +0800

    Update Bilangan ganjil genap.py

--- Bilangan ganjil genap.py
+++ Bilangan ganjil genap.py
@@ -1,1 +1,8 @@
+# Minta input dari pengguna
+bilangan = int(input("Masukkan bilangan: "))
 
+# Periksa apakah bilangan tersebut genap atau ganjil
+if bilangan % 2 == 0:
+    print(f"{bilangan} adalah bilangan genap.")
+else:
+    print(f"{bilangan} adalah bilangan ganjil.")
